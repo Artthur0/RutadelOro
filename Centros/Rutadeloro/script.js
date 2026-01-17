6ad9713314216bfb9eef08769b86788978f0ec90
Artthur0 <vael.253@gmail.com>
2026-01-17 18:07:29 -0300

    mejoras

--- Centros/Rutadeloro/script.js
+++ Centros/Rutadeloro/script.js
@@ -55,4 +55,41 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
+  const leerMasBtn = document.querySelector(".leer-mas-btn");
+  const espaciosExtra = document.querySelector(".espacios-extra");
+
+  leerMasBtn.addEventListener("click", () => {
+    const visible = espaciosExtra.style.display === "block";
+
+    espaciosExtra.style.display = visible ? "none" : "block";
+    leerMasBtn.textContent = visible ? "Leer más" : "Leer menos";
+  });
+
+
+});
+
+
+const modal = document.getElementById("modal-servicios");
+const openBtn = document.querySelector(".ver-servicios-btn");
+const closeBtn = document.querySelector(".modal-close");
+
+// Abrir modal
+openBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
+
+// Cerrar modal
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+});
+
+// Cerrar al hacer clic fuera
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  }
 });
